e3638ead4d53a23692580cfdc96c58b6f6dd4ad9
Peter Smirnov <petr.smirnowv@gmail.com>
2024-06-30 22:15:40 +0300

    f1

--- src/main/java/ru/t1/tasks/App.java
+++ src/main/java/ru/t1/tasks/App.java
@@ -1,47 +1,79 @@
 package ru.t1.tasks;
-
-import cn.hutool.json.JSONObject;
-
-import io.netty.channel.ChannelOption;
-
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import reactor.netty.http.client.HttpClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-import java.util.Base64;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+@SpringBootApplication
+public class App {
 
-
-public class App
-{
-
-    /*private static final WebClient client = WebClient.create();*/
-
-    public static void doPostAddStudent(){
-        throw new RuntimeException();
-
-    }
-    public static JSONObject doGetCode(){
-        throw new RuntimeException();
-    }
-    public static String getStringToIncreaseStatus(String email, String code) {
-        String emailCode = email.concat(":").concat(code);
-        return Base64.getEncoder().encodeToString(emailCode.getBytes());
-    }
-
-    public static void doPostIncreaseTheLvl(){
-
-    }
-    public static void main( String[] args )
-    {
-        /*HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-                .responseTimeout(Duration.ofMillis(5000));*/
-
+    public static void main(String[] args) throws MalformedURLException {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
-        RoleService roleService = context.getBean(RoleService.class);
-        MyService myService = new MyService(roleService);
-        myService.useRoles();
+
+        /*WebClient webClient = context.getBean(WebClient.Builder.class).build();*/
+        WebClient webClient = WebClient.create();
+
+        /*fetchData(webClient, "http://193.19.100.32:7000/api/get-roles")
+                .subscribe(response -> {
+                    System.out.println("Получен ответ: " + response);
+                }, error -> {
+                    System.err.println("Ошибка при выполнении запроса: " + error.getMessage());
+                });*/
+
+        String hostname = "193.19.100.32";
+        int port = 7000;
+        String apiPath = "/api/get-code";
+        String email = "petr.smirnowv@gmail.com";
+        String urlWithParams = String.format("http://%s:%d%s?email=%s",
+                hostname, port, apiPath, email);
+        System.out.println(urlWithParams);
+        fetchDataWithParam(webClient, email)
+                .doOnSuccess(response -> {
+                    System.out.println("Запрос успешно выполнен: " + response);
+                })
+                .subscribe(response -> {
+                    System.out.println("Ответ: " + response);
+                }, error ->{
+                    System.out.println("Ошибка");
+                });
+
+
+        /*fetchDataWithParam(webClient, "petr.smirnowv1@gmail.com")
+                .doOnSuccess(response -> {
+                    System.out.println("Запрос успешно выполнен: " + response);
+                })
+                .subscribe(response -> {
+                    System.out.println("Получен ответ: " + response);
+                }, error -> {
+                    System.err.println("Ошибка при выполнении запроса: " + error.getMessage());
+                });
+
+    }*/
     }
+    public static Mono<String> fetchData(WebClient webClient, String url) {
+        return webClient.get()
+                .uri(url)
+                .header("Accept", "text/plain")
+                .retrieve()
+                .bodyToMono(String.class); // Или другой тип данных
+    }
+    public static Mono<String> fetchDataWithParam(WebClient webClient, String email) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host("193.19.100.32")
+                        .port(7000)
+                        .path("/api/get-code")
+                        .queryParam("email", email)
+                        .build())
+                .header("Accept", "text/plain")
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
 }
